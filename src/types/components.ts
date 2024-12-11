eae32ef1874d24a3f2e78e3ccb6e2878f3c1f17b
jakpro657 <jakepro657@gmail.com>
2024-12-12 01:56:29 +0900

    feat: article type

--- src/types/components.ts
+++ src/types/components.ts
@@ -10,28 +10,33 @@ export type BookType = {
   href: string;
 };
 
-export type ArticleType = {
-  title: string;
-  content: string;
-  createdAt: string;
-  like: number;
-  commentCount: number;
-  comment?: CommentType[];
-  image?: string;
-  user: UserType;
-};
-
 export type CommentType = {
   content: string;
   createdAt: string;
   user: UserType;
 };
 
-export type UserType = {
+export interface UserType {
+  id: number;
+  profile: string;
   name: string;
-  email: string;
-  image?: string;
-};
+  role: string; // 필요하다면 "USER" | "ADMIN" 등으로 세분화 가능
+  is_deleted: boolean;
+}
+
+export interface ArticleType {
+  id: number;
+  user_id: number;
+  title: string;
+  content: string;
+  image1: string;
+  image2: string;
+  likes_num: number;
+  comments_num: number;
+  created_at: string; // "2024-12-09T10:38:26.033Z" 처럼 ISO8601 포맷 시간 문자열
+  updated_at: string;
+  user: UserType;
+}
 
 export type SidebarType = {
   user: UserType;
