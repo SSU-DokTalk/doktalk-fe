e77fefabba3c120c76da56c426d150d24e849b6b
HJH <nahjh2@gmail.com>
2025-02-22 23:55:42 +0900

    set css injection order & css baseline
    https://mui.com/material-ui/integrations/interoperability/#css-injection-order
    https://mui.com/material-ui/react-css-baseline/

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { StyledEngineProvider } from "@mui/material/styles";
+
 import { store } from "@/stores/store.ts";
 
 import "@/locales/i18n.ts";
@@ -9,13 +13,15 @@ import App from "@/App.tsx";
 import TokenRefresher from "@/TokenRefresher";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter
-    future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-  >
-    <Provider store={store}>
-      <TokenRefresher>
-        <App />
-      </TokenRefresher>
-    </Provider>
-  </BrowserRouter>
+  <StyledEngineProvider injectFirst>
+    <CssBaseline />
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <Provider store={store}>
+        <TokenRefresher>
+          <App />
+        </TokenRefresher>
+      </Provider>
+    </BrowserRouter>
+  </StyledEngineProvider>
 );
