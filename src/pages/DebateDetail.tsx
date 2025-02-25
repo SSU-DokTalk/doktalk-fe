295af75aecd04c3f4bae8cc51dd9ed49c8c9fb2b
HJH <nahjh2@gmail.com>
2025-02-26 02:30:08 +0900

    [#2] debate detail page

--- src/pages/DebateDetail.tsx
+++ src/pages/DebateDetail.tsx
@@ -2,7 +2,12 @@ import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons/faCartPlus";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faChevronRight,
+  faPen,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import ProfileIcon from "@/components/base/ProfileIcon";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -12,12 +17,22 @@ import { getDateTime, getTimeDiff } from "@/functions";
 import Image from "@/components/base/Image";
 import { selectUser } from "@/stores/user";
 import { useAppSelector } from "@/stores/hooks";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Paper,
+} from "@mui/material";
 
 function DebateDetail() {
   const { debate_id } = useParams();
   const [debate, setDebate] = useState<DebateType>(InitialDebate);
 
   const [purchaseId, setPurchaseId] = useState<number>(0);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const user = useAppSelector(selectUser);
 
@@ -73,99 +88,149 @@ function DebateDetail() {
     );
   };
 
+  function doDelete(event: MouseEvent<HTMLDivElement, MouseEvent>): void {
+    alert("미구현입니당");
+    // throw new Error("Function not implemented.");
+  }
+
   return (
-    <div id="debate-detail">
-      <div className="user-header">
-        <div className="user-header__info">
+    <div id='debate-detail'>
+      <div className='user-header'>
+        <div className='user-header__info'>
           <ProfileIcon
             profile={debate.user.profile}
             size={40}
-            className="user-header__avatar"
+            className='user-header__avatar'
           />
-          <div className="user-header__text">
-            <span className="user-header__nickname">{debate.user.name}</span>
-            <span className="user-header__time">
+          <div className='user-header__text'>
+            <span className='user-header__nickname'>{debate.user.name}</span>
+            <span className='user-header__time'>
               {getTimeDiff(debate.created)}
             </span>
           </div>
         </div>
 
-        <div className="user-header__actions">
-          {user.id === debate.user.id && (
-            <>
-              <button>수정</button>
-              <button className="delete">삭제</button>
-            </>
-          )}
+        <div className='user-header__actions'>
+          <div className='for-pc hidden md:block'>
+            {user.id === debate.user.id && (
+              <>
+                <button>수정</button>
+                <button className='delete'>삭제</button>
+              </>
+            )}
+          </div>
+
+          <div className='for-mobile md:hidden'>
+            <IconButton
+              className='drawer-button md:hidden!'
+              onClick={() => setDrawerOpen(true)}>
+              <FontAwesomeIcon icon={faBars} />
+            </IconButton>
+            <Drawer
+              anchor='bottom'
+              open={drawerOpen}
+              onClose={() => setDrawerOpen(false)}
+              PaperProps={{
+                sx: {
+                  bgcolor: "transparent",
+                },
+              }}>
+              <Paper className='rounded-t-2xl!'>
+                <hr className='w-15 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm dark:bg-gray-700' />
+                <p className='text-center'>게시글 옵션</p>
+                <List>
+                  <ListItem>
+                    <ListItemButton
+                      className='edit'
+                      onClick={() => alert("미구현입니당")}>
+                      <ListItemIcon>
+                        <FontAwesomeIcon icon={faPen} />
+                      </ListItemIcon>
+                      수정
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemButton className='delete' onClick={doDelete}>
+                      <ListItemIcon>
+                        <FontAwesomeIcon icon={faTrash} />
+                      </ListItemIcon>
+                      삭제
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+              </Paper>
+            </Drawer>
+          </div>
         </div>
       </div>
-      <div className="discussion-info">
-        <h2 className="discussion-info__title">{debate.title}</h2>
 
-        <div className="discussion-info__grid">
-          <div className="discussion-info__item">
+      <div className='discussion-info'>
+        <h2 className='discussion-info__title'>{debate.title}</h2>
+
+        <div className='discussion-info__grid'>
+          <div className='discussion-info__item'>
             <strong>모임 장소</strong>
             <span>{debate.location}</span>
           </div>
-          <div className="discussion-info__item">
+          <div className='discussion-info__item'>
             <strong>온라인 링크</strong>
             <span>{debate.link}</span>
           </div>
-          <div className="discussion-info__item">
+          <div className='discussion-info__item'>
             <strong>시간</strong>
             <span>{getDateTime(new Date(debate.held_at))}</span>
           </div>
-          <div className="discussion-info__item">
+          <div className='discussion-info__item'>
             <strong>카테고리</strong>
             <span>인문</span>
           </div>
-          <div className="discussion-info__item book">
+          <div className='discussion-info__item book'>
             <strong>지정 도서</strong>
             <div>
               <span>{debate.book.title}</span>
               <Image
                 src={debate.book.image}
-                width="100px"
-                height="140px"
-                className="discussion-info__cover"
+                width='100px'
+                height='140px'
+                className='discussion-info__cover'
               />
             </div>
           </div>
         </div>
 
-        <pre className="discussion-info__description">{debate.content}</pre>
+        <pre className='discussion-info__description'>{debate.content}</pre>
 
-        <div className="discussion-info__likes">
-          <FontAwesomeIcon icon={faHeart} className="like-icon" />
-          <span className="discussion-info__likes__text">
+        <div className='discussion-info__likes'>
+          <FontAwesomeIcon icon={faHeart} className='like-icon' />
+          <span className='discussion-info__likes__text'>
             {debate.likes_num}
           </span>
         </div>
       </div>
-      <div className="payment-box">
-        <p className="payment-box__title">
+      <div className='payment-box'>
+        <p className='payment-box__title'>
           토론방에 참여하시려면 결제가 필요합니다.
         </p>
 
-        <div className="payment-box__info">
+        <div className='payment-box__info'>
           <button
-            className="payment-box__currency"
+            className='payment-box__currency'
             onClick={() => cancelPurchase()} // 결제 테스트용
           >
             <FontAwesomeIcon icon={faCartPlus} />
             {""} 찜
           </button>
           <span
-            className="payment-box__amount"
+            className='payment-box__amount'
             onClick={getPurchase} // 결제 테스트용
           >
             1,000 원
           </span>
-          <button className="payment-box__button" onClick={doPurchase}>
+          <button className='payment-box__button' onClick={doPurchase}>
             결제하기{" "}
             <FontAwesomeIcon
               icon={faChevronRight}
-              className="payment-box__button-icon"
+              className='payment-box__button-icon'
             />
           </button>
         </div>
