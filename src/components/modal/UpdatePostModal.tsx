dbc270782e31b43192719f80ef7d852d2277e68e
HJH <nahjh2@gmail.com>
2025-04-07 20:47:06 +0900

    fix build error

--- src/components/modal/UpdatePostModal.tsx
+++ src/components/modal/UpdatePostModal.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 import { Dialog, DialogTitle, DialogContent, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
