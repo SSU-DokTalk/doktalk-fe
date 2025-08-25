7979539f3a836dd3eac58cee169c2dada9ac5b64
HJH <nahjh2@gmail.com>
2025-08-25 16:34:03 +0900

    [#56] reorder top navigation menus

--- src/components/navigation/Topnav.tsx
+++ src/components/navigation/Topnav.tsx
@@ -49,11 +49,6 @@ const navTabs: LinkItemType[] = [
     icon: <FontAwesomeIcon icon={faHouse} />,
   },
   {
-    title: 'component.topnav.post',
-    url: '/post',
-    icon: <FontAwesomeIcon icon={faFileLines} />,
-  },
-  {
     title: 'component.topnav.debate',
     url: '/debate',
     icon: <FontAwesomeIcon icon={faComments} />,
@@ -61,6 +56,11 @@ const navTabs: LinkItemType[] = [
   {
     title: 'component.topnav.summary',
     url: '/summary',
+    icon: <FontAwesomeIcon icon={faFileLines} />,
+  },
+  {
+    title: 'component.topnav.post',
+    url: '/post',
     icon: <FontAwesomeIcon icon={faFileLines} />,
   },
   {
