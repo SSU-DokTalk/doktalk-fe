6b49496ef390186dad9aa69514d9d24887317afa
HJH <nahjh2@gmail.com>
2025-10-17 14:50:27 +0900

    change layout

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,10 +9,15 @@ import axios from 'axios';
 import { MiddlePanel, RightPanel } from '@/components/panel/sidePanel';
 import SimplePostCard from '@/components/card/SimplePostCard';
 import { DebateType } from '@/types/data';
-import { DUMMY_DEBATES } from '@/common/dummy_data';
+import { DUMMY_DEBATES, DUMMY_SUMMARIES } from '@/common/dummy_data';
+import Carousel from '@/components/carousel/Carousel';
+import CarouselDebateCard from '@/components/card/CarouselDebateCard';
+import { isMd } from '@/functions/breakpoint';
+import CarouselSummaryCard from '@/components/card/CarouselSummaryCard';
 
 function Landing() {
-  const [debates, _] = useState<DebateType[]>(DUMMY_DEBATES);
+  const [recommendDebates, ..._] = useState<DebateType[]>(DUMMY_DEBATES);
+  const [recommendSummaries, ...__] = useState<SummaryType[]>(DUMMY_SUMMARIES);
 
   const [posts, setPosts] = useState<PostType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -80,78 +85,61 @@ function Landing() {
 
   return (
     <div id='landing-page' className='flex flex-col'>
-      <div
-        id='landing-page-upper-container'
-        className='pb-8! mb-8! md:pb-15! md:mb-15!'
-      >
-        <div className='container-title md:ml-8! md:px-24! px-4!'>
-          {t('page.landing.title.recommend-prefix')}
-          {user.id == 0 ? null : (
-            <span>
-              {t('page.landing.title.for-you-prefix')}
-              <span className='user-name'>{user.name}</span>
-              {t('page.landing.title.for-you-postfix')}
-            </span>
-          )}
-          {t('page.landing.title.recommend-postfix')}
+      <div className='recommendation-section'>
+        <div className='recommended-debates md:pl-24! pr-12 mb-8'>
+          <div className='recommend-content-container md:my-9!'>
+            <div className='recommend-content-title ml-6!'>
+              {t('page.debate.title.recommend')}
+            </div>
+            <Carousel size={3} className='recommend-content mx-auto! md:m-0!'>
+              {recommendDebates.map((debate, idx) => (
+                <CarouselDebateCard
+                  key={'recommend-debate' + idx}
+                  debate={debate}
+                />
+              ))}
+            </Carousel>
+          </div>
         </div>
 
-        <div className='container-contents-carousel flex items-end justify-items-center gap-4 overflow-x-auto w-full md:px-24! px-4!'>
-          {debates.map((debate, idx) => (
-            <SimplePostCard
-              key={'recommend-debate' + idx}
-              title={debate.title}
-              content={debate.content}
-              thumbnail={debate.book.image}
-              navigateTo={`/debate/${debate.id}`}
-              width='group w-44 hover:w-52 flex-shrink-0 transition-all duration-300 ease-in-out'
-              objectFit='cover'
-              imageHeight='h-64 group-hover:h-76 transition-all duration-300 ease-in-out'
-            />
-          ))}
+        <div className='popular-content-container md:my-9!'>
+          <div className='popular-content-title ml-6!'>
+            {t('page.summary.title.recommend')}
+          </div>
+          <Carousel size={3}>
+            {recommendSummaries.map((summary, index) => (
+              <CarouselSummaryCard
+                key={'popular-summary' + index}
+                summary={summary}
+              />
+            ))}
+          </Carousel>
         </div>
       </div>
 
       <div className='flex flex-row md:pl-24! pr-12'>
-        <MiddlePanel className='post-container px-4! w-full'>
-          <div className='container mx-auto px-4 py-8'>
-            <h1 className='text-3xl! font-bold! text-brand1 mb-6!'>
-              {t('page.landing.title.post')}
-            </h1>
-            {posts.length === 0 ? (
-              <p className='text-gray-600'>
-                {t('page.landing.item.no-post-item')}
-              </p>
-            ) : (
-              <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2'>
-                {posts.map((post) => (
-                  <SimplePostCard
-                    key={post.id}
-                    title={post.title}
-                    content={post.content}
-                    thumbnail={post.files?.[0].url}
-                    navigateTo={`/post/${post.id}`}
-                  />
-                ))}
-              </div>
-            )}
-          </div>
-        </MiddlePanel>
-
-        <RightPanel>
-          <div className='summary-container ml-4! mr-8!'>
-            <div className='summary-section-title'>
-              {t('page.landing.title.popular-summary')}
+        <div className='container mx-auto px-4 py-8'>
+          <h1 className='text-3xl! font-bold! text-brand1 mb-6!'>
+            {t('page.landing.title.post')}
+          </h1>
+          {posts.length === 0 ? (
+            <p className='text-gray-600'>
+              {t('page.landing.item.no-post-item')}
+            </p>
+          ) : (
+            <div className=''>
+              {posts.map((post) => (
+                <SimplePostCard
+                  key={post.id}
+                  title={post.title}
+                  content={post.content}
+                  thumbnail={post.files?.[0].url}
+                  navigateTo={`/post/${post.id}`}
+                />
+              ))}
             </div>
-            {popularSummaries.map((summary, index) => (
-              <PopularSummaryCard
-                key={'popular-summary' + index}
-                summary={summary}
-                showInfo={false}
-              />
-            ))}
-          </div>
-        </RightPanel>
+          )}
+        </div>
       </div>
     </div>
   );
